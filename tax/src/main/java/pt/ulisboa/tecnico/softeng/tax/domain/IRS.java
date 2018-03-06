51b1ac82ffd5f94df86e84fd983c217ed1699463
RafaelRibeiro97 <rafaelmmribeiro@gmail.com>
2018-03-06 16:57:55 +0000

    added argument verification and data struct InvoiceData (#9)

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/IRS.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/IRS.java
@@ -3,20 +3,28 @@ package pt.ulisboa.tecnico.softeng.tax.domain;
 import java.util.HashSet;
 import java.util.Set;
 
+import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
 import pt.ulisboa.tecnico.softeng.tax.domain.TaxPayer;
 import pt.ulisboa.tecnico.softeng.tax.domain.ItemType;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
 
-public class IRS {
+public final class IRS {
 	public static Set<TaxPayer> _taxPayers = new HashSet<TaxPayer>();
 	public static Set<ItemType> _itemTypes = new HashSet<ItemType>();
 
-	public ItemType getItemTypeByName(String type) { 
+	private IRS() {  }
+
+	public static ItemType getItemTypeByName(String type) { 
 		return new ItemType();
 	}
 
-	public TaxPayer getTaxPayerByNIF(String buyerNIF) {
+	public static TaxPayer getTaxPayerByNIF(String buyerNIF) throws TaxException {
+		if (buyerNIF.length() != TaxException.NIF_SIZE) {
+			throw new TaxException();
+		}
+
 		return null;
 	}
 
-	public void submitInvoice() {  }
+	public static void submitInvoice(InvoiceData invoice) {  }
 }
