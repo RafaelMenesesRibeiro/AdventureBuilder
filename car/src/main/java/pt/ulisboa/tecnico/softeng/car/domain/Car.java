a1b1312519caab2e0736c5141051e95d1db54b51
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2018-03-06 19:09:05 +0000

    Local nuke

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Car.java
+++
@@ -1,9 +0,0 @@
-package pt.ulisboa.tecnico.softeng.car.domain;
-
-import pt.ulisboa.tecnico.softeng.car.dataobjects.RentingData;
-import pt.ulisboa.tecnico.softeng.car.exception.CarException;
-import pt.ulisboa.tecnico.softeng.car.domain.Vehicle;
-
-public class Car extends Vehicle {
-
-}
