4f1a571a4c5384045a4769554607da3f9a92aac4
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2018-03-07 19:25:44 +0000

    Car: OK

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Motorcycle.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Motorcycle.java
@@ -5,15 +5,11 @@ import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 import pt.ulisboa.tecnico.softeng.car.domain.Vehicle;
 
 public class Motorcycle extends Vehicle {
-
 	public Motorcycle(String plate, RentACar dealer) {
 		super(plate, dealer);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Motorcycle(String plate, RentACar dealer, int kilometers) {
-		super(plate, dealer, kilometers);
-		// TODO Auto-generated constructor stub
 	}
 
+	public Motorcycle(String plate, int kilometers, RentACar dealer) {
+		super(plate, kilometers, dealer);
+	}
 }
