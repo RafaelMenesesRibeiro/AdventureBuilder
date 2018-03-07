c11349d66fcc7ab0404c26cc3b2ac2079bd217d0
Andreia Valente <andreiaavalentee@gmail.com>
2018-03-07 18:34:15 +0000

    added constructor method tests for Seller and Buyer

---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/BuyerConstructorMethodTest.java
@@ -0,0 +1,66 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
+public class BuyerConstructorMethodTest {
+	private static final String BUYER_NIF = "177777777";
+	private static final String BUYER_NAME = "Antonio Sarmento";
+	private static final String BUYER_ADDRESS = "Estrela da lapa";
+
+
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void success() {
+		Buyer buyer = new Buyer(BUYER_NIF, BUYER_NAME,BUYER_ADDRESS);
+
+		Assert.assertEquals(BUYER_NIF, buyer.getNif());
+		Assert.assertEquals(BUYER_NAME, buyer.getName());
+		Assert.assertEquals(BUYER_ADDRESS, buyer.getAddress());
+		
+		Assert.assertTrue(buyer.getNif().length() > TaxPayer.NIF_SIZE);
+	}
+
+	@Test(expected = TaxException.class)
+	public void nullBuyerNif() {
+		new Buyer(null, BUYER_NAME, BUYER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void emptyBuyerNif() {
+		new Buyer("    ", BUYER_NAME, BUYER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void nullBuyerName() {
+		new Buyer(BUYER_NIF, null, BUYER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void emptyBuyerName() {
+		new Buyer(BUYER_NIF, "    ", BUYER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void nullBuyerAddress() {
+		new Buyer(BUYER_NIF, BUYER_NAME, null);
+	}
+
+	@Test(expected = TaxException.class)
+	public void emptyBuyerName() {
+		new Buyer(BUYER_NIF, BUYER_NAME, "    ");
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+
+}
---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/SellerConstructorMethodTest.java
@@ -0,0 +1,66 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
+public class SellerConstructorMethodTest {
+	private static final String SELLER_NIF = "111111111";
+	private static final String SELLER_NAME = "Joao Antonio";
+	private static final String SELLER_ADDRESS = "Rua dos Vendedores";
+
+
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void success() {
+		Seller seller = new Seller(SELLER_NIF, SELLER_NAME,SELLER_ADDRESS);
+
+		Assert.assertEquals(SELLER_NIF, Seller.getNif());
+		Assert.assertEquals(SELLER_NAME, Seller.getName());
+		Assert.assertEquals(SELLER_ADDRESS, Seller.getAddress());
+		
+		Assert.assertTrue(Seller.getNif().length() > TaxPayer.NIF_SIZE);
+	}
+
+	@Test(expected = TaxException.class)
+	public void nullSellerNif() {
+		new Seller(null, SELLER_NAME, SELLER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void emptySellerNif() {
+		new Seller("    ", SELLER_NAME, SELLER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void nullSellerName() {
+		new Seller(SELLER_NIF, null, SELLER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void emptySellerName() {
+		new Seller(SELLER_NIF, "    ", SELLER_ADDRESS);
+	}
+
+	@Test(expected = TaxException.class)
+	public void nullSellerAddress() {
+		new Seller(SELLER_NIF, SELLER_NAME, null);
+	}
+
+	@Test(expected = TaxException.class)
+	public void emptySellerName() {
+		new Seller(SELLER_NIF, SELLER_NAME, "    ");
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+
+}
