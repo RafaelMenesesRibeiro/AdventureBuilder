d3cc74726c56bc2a82d246293c2ca335ab469666
Andreia Valente <andreiaavalentee@gmail.com>
2018-03-08 16:31:59 +0000

    Added item type constructor method test (incomplete)

---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/ItemTypeConstructorMethodTest.java
@@ -0,0 +1,37 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
+public class ItemTypeConstructorMethodTest {
+	private static final int TAX = 23;
+	private static final String ITEM_TYPE = "Videojogo";
+
+
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void success() {
+		ItemType itemType = new ItemType(ITEM_TYPE, TAX);
+
+		Assert.assertEquals(ITEM_TYPE, itemType.getItemTypeName());
+		Assert.assertEquals(TAX, itemType.getTax());
+
+		Assert.assertTrue(itemType.getNumberOfInvoices() >= 1);	
+		Assert.assertTrue(IRS.getNumberOfItems() >= 1);		
+	}
+
+
+	@After
+	public void tearDown() {
+		ItemType.invoices.clear();
+	}
+
+
+}
