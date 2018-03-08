b914f8f242c90e9073074d5611a9fe9066d87613
Andreia Valente <andreiaavalentee@gmail.com>
2018-03-08 17:03:33 +0000

    Fixed float problem on invoice  constructor method test (incomplete)

--- tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/InvoiceConstructorMethodTest.java
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/InvoiceConstructorMethodTest.java
@@ -1,6 +1,6 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
-import org.joda.time;
+import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,9 +9,9 @@ import org.junit.Test;
 import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
 
 public class InvoiceConstructorMethodTest {
-	private static final float INVOICE_VALUE = 1.00;
+	private static final float INVOICE_VALUE = 1.00f;
 	private static final String ITEM_TYPE = "Roupa";
-	private final LocalDate date;
+	private LocalDate date;
 	private Buyer buyer;
 	private Seller seller;
 	private ItemType itemType;
@@ -30,10 +30,10 @@ public class InvoiceConstructorMethodTest {
 	public void success() {
 		Invoice invoice = new Invoice(INVOICE_VALUE, this.date, ITEM_TYPE, this.seller, this.buyer);
 
-		Assert.assertEquals(INVOICE_VALUE, invoice.getValue());
+		Assert.assertEquals(INVOICE_VALUE, invoice.getValue(), 0.0f);
 		Assert.assertEquals(this.date, invoice.getDate());
 		Assert.assertTrue(invoice.getValue() > 0);
-		Assert.assertTrue(invoice.getYear() > 1970);
+		Assert.assertTrue(invoice.getDate().getYear() > 1970);
 	}
 
 
