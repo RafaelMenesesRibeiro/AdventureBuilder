a6802eee4c1bc5d748a64eb833b5db6411a4992c
diogofsvilela <diogofsvilela@gmail.com>
2018-03-08 11:54:30 +0000

    Added car module skeleton

---
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/VehicleIsFreeMethodTest.java
@@ -0,0 +1,27 @@
+package pt.ulisboa.tecnico.softeng.car.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.car.exception.CarException;
+
+public class VehicleIsFreeMethodTest {
+	
+	@Before
+	public void setUp() {
+
+    }
+
+	@Test
+	public void success() {
+
+	}
+	
+	@After
+	public void tearDown() {
+
+	}
+	
+}
