93d94f4def5a86f190924dde6fc0e4cfb979cd6c
diogofsvilela <diogofsvilela@gmail.com>
2018-03-08 11:24:51 +0000

    Expected expection cases added to RentACarGetRentingMethodTest

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingMethodTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingMethodTest.java
@@ -1,39 +1,59 @@
 package pt.ulisboa.tecnico.softeng.car.domain;
 
-import java.time.LocalDateTime;
+import org.joda.time.LocalDate;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.car.exception.CarException;
+
 public class RentACarGetRentingMethodTest {
 	
 	private String drivingLicense;
-	private LocalDateTime begin;
-	private LocalDateTime end;
+	private LocalDate begin;
+	private LocalDate end;
 	private RentACar renter;
 	private Vehicle vehicle;
+	private Renting renting;
 	
 	@Before
 	public void setUp() {
 		this.drivingLicense = "VC12345";
-		this.begin = LocalDateTime.now();
+		this.begin = LocalDate.now();
 		this.end = this.begin.plusDays(1);
 		this.renter = new RentACar("Renter");
 		this.vehicle = new Vehicle("ZZ-ZZ-ZZ", this.renter);
+		this.renting = this.vehicle.rent(this.drivingLicense, this.begin, this.end);
 	}
 	
 	@Test
 	public void success() {
-		this.vehicle.rent(this.drivingLicense, this.begin, this.end);
-		Renting renting = this.vehicle.getRent(this.begin, this.end);
-		Renting result = this.renter.getRenting(renting.getReferece());
-		Assert.assertEquals(renting, result);
+		Renting result = this.renter.getRenting(renting.getReference());
+		Assert.assertEquals(this.renting, result);
 	}
 	
-	//@Test (expected = )
-	
+	@Test (expected = CarException.class)
+	public void notNullReferenceArgument() {
+		this.renter.getRenting(null);
+	}
+
+	@Test (expected = CarException.class)
+	public void notEmptyReferenceArgument() {
+		this.renter.getRenting("");
+	}
+
+	@Test (expected = CarException.class)
+	public void notBlankReferenceArgument() {
+		this.renter.getRenting("  ");
+	}
+
+	@Test (expected = CarException.class)
+	public void noMatchReferenceArgument() {
+		this.renter.getRenting("XPTO");
+	}
+
 	@After
 	public void tearDown() {
 		
