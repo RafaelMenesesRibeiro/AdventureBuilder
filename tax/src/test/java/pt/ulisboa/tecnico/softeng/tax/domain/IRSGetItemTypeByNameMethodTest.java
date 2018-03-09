4144756f84ee655bd34576d90bf8b6389664fe96
Andreia Valente <andreiaavalentee@gmail.com>
2018-03-09 21:58:29 +0000

    Added file of test for IRS' method getItemTypeByName (issue #14)

---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/IRSGetItemTypeByNameMethodTest.java
@@ -0,0 +1,58 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
+public class IRSGetItemTypeByNameMethodTest {
+	List<ItemType> itemTypes;
+	ItemType bolacha;
+	ItemType brinquedo;
+	ItemType cerveja;
+
+	@Before
+	public void setUp() {
+		this.bolacha = new ItemType ("Bolacha", 10);
+		this.brinquedo = new ItemType ("Brinquedo", 40);
+		this.cerveja = new ItemType ("Cerveja", 10);
+	}
+
+	@Test
+	public void success() {
+
+		this.itemTypes = IRS.getItemTypes();
+		int index = 0;
+		for (ItemType type: this.itemTypes) {
+			if (type.getName().equals("Brinquedo")) {
+				Assert.assertEquals(type, IRS.getItemTypeByName("Brinquedo"));
+			}
+		}
+	}
+
+	@Test(expected = TaxException.class)
+	public void nullName() {
+		IRS.getItemTypeByName(null);
+	}
+
+	@Test(expected = TaxException.class)
+	public void emptyName() {
+		IRS.getItemTypeByName("");
+	}
+
+	@Test(expected = TaxException.class)
+	public void nameNotExists() {
+		IRS.getItemTypeByName("Videojogo");
+	}
+
+
+	@After
+	public void tearDown() {
+	}
+
+
+}
