9afaaf2352f4426d1cea57ff743ab57e945a3b96
diogofsvilela <diogofsvilela@gmail.com>
2018-03-09 09:06:51 +0000

    RentACarGetRentingDataMethodTest first draft

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingDataMethodTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingDataMethodTest.java
@@ -1,22 +1,40 @@
 package pt.ulisboa.tecnico.softeng.car.domain;
 
+import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.car.dataobjects.RentingData;
 import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 
 public class RentACarGetRentingDataMethodTest {
-
+	private String drivingLicense;
+	private LocalDate begin;
+	private LocalDate end;
+	private RentACar renter;
+	private Vehicle vehicle;
+	private Renting renting;	
 	@Before
 	public void setUp() {
-
+		this.drivingLicense = "VC12345";
+		this.begin = LocalDate.now();
+		this.end = this.begin.plusDays(1);
+		this.renter = new RentACar("Renter");
+		this.vehicle = new Vehicle("ZZ-ZZ-ZZ", 0, this.renter);
+		this.renting = this.vehicle.rent(this.drivingLicense, this.begin, this.end);
 	}
 
 	@Test
 	public void success() {
-
+		RentingData rentingData = this.renter.getRentingData(this.renting.getReference());
+		Assert.assertEquals(rentingData.getReference(), this.renting.getReference());
+		Assert.assertEquals(rentingData.getPlate(), this.vehicle.getPlate());
+		Assert.assertEquals(rentingData.getDrivingLicense(), this.drivingLicense);
+		Assert.assertEquals(rentingData.getRentACarCode(), this.renter.getCode());
+		Assert.assertEquals(rentingData.getBeginDate(), this.begin);
+		Assert.assertEquals(rentingData.getEndDate(), this.end);
 	}
 
 	@After
