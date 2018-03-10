c19af1c4deb45ede5b664c0ada67380ee22533e4
RafaelRibeiro97 <rafaelmmribeiro@gmail.com>
2018-03-10 02:12:09 +0000

    Fixed implementation of taxReturn(year) in Buyer. (references #9 #14)

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Buyer.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Buyer.java
@@ -15,18 +15,16 @@ public class Buyer extends TaxPayer{
 
 	public float taxReturn(int year) throws TaxException {
 		Map<String, Invoice> invoices = this.getInvoices();
-
 		float taxReturn = 0.00f;
-
 		for (Iterator iter = invoices.entrySet().iterator(); iter.hasNext(); ) {
 			Map.Entry pair = (Map.Entry) iter.next();
 			Invoice invoice = (Invoice) pair.getValue();
 			if (invoice.getDate().getYear() == year) {
-				float invoiceIVA = invoice.getIVA();
-				taxReturn += invoiceIVA * 0.05;
+				float invoiceValue = invoice.getValue();
+				int invoiceTax = invoice.getItemType().getTax();
+				taxReturn += (float) invoiceValue * invoiceTax * 0.05;
 			}
 		}
-
 		return taxReturn;
 	}
 }
