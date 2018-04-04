d1bea48495890a20993eab110879bd68c95645a6
diogofsvilela <diogofsvilela@gmail.com>
2018-04-04 16:17:57 +0100

    Changing the way submitInvoice is called - Closes #56 #57

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/interfaces/TaxInterface.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/interfaces/TaxInterface.java
@@ -3,11 +3,13 @@ package pt.ulisboa.tecnico.softeng.car.interfaces;
 import pt.ulisboa.tecnico.softeng.tax.domain.IRS;
 import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
 
-public class TaxInterface {
-	public static String submitInvoice(InvoiceData invoiceData) {
-        return IRS.submitInvoice(invoiceData);
-    }
+import org.joda.time.LocalDate;
 
+public class TaxInterface {
+    public static String submitInvoice(String sellerNIF, String buyerNIF, String itemType, double value, LocalDate date) {
+        return IRS.submitInvoice(new InvoiceData(sellerNIF, buyerNIF, itemType, value, date));
+    }
+    
     public static String cancelInvoice(String referString) {
         return IRS.cancelInvoice(referString);
     }
--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/interfaces/TaxInterface.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/interfaces/TaxInterface.java
@@ -3,9 +3,11 @@ package pt.ulisboa.tecnico.softeng.hotel.interfaces;
 import pt.ulisboa.tecnico.softeng.tax.domain.IRS;
 import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
 
+import org.joda.time.LocalDate;
+
 public class TaxInterface {
-	public static String submitInvoice(InvoiceData invoiceData) {
-        return IRS.submitInvoice(invoiceData);
+    public static String submitInvoice(String sellerNIF, String buyerNIF, String itemType, double value, LocalDate date) {
+        return IRS.submitInvoice(new InvoiceData(sellerNIF, buyerNIF, itemType, value, date));
     }
 
     public static String cancelInvoice(String referString) {
