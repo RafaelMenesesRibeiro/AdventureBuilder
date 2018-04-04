2a6f4220862d3a40fb45d29ff0e1bd83e7360027
diogofsvilela <diogofsvilela@gmail.com>
2018-04-04 14:47:55 +0100

    Added submitInvoice to tax interface in car and hotel module - References #56 & #57

---
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/interfaces/TaxInterface.java
@@ -0,0 +1,10 @@
+package pt.ulisboa.tecnico.softeng.hotel.interfaces;
+
+import pt.ulisboa.tecnico.softeng.tax.domain.IRS;
+import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
+
+public class TaxInterface {
+	public static String submitInvoice(InvoiceData invoiceData) {
+        return IRS.submitInvoice(invoiceData);
+    }
+}
