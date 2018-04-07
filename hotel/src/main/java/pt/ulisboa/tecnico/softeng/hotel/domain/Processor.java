0e3a157730a59f2c0c6d9eae57cc89ed3eff47e3
Diogo Redin <diogo.redin@gmail.com>
2018-04-07 16:00:22 +0100

    Processes payment in bank in hotel (#23 e #26)

---
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/domain/Processor.java
@@ -0,0 +1,49 @@
+package pt.ulisboa.tecnico.softeng.hotel.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import pt.ulisboa.tecnico.softeng.hotel.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.hotel.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.hotel.interfaces.TaxInterface;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+
+public class Processor {
+
+	// important to use a set to avoid double submission of the same booking when it
+	// is cancelled while trying to pay or send invoice
+	private final Set<Booking> bookingToProcess = new HashSet<>();
+
+	public void submitRenting(Booking booking) {
+		this.bookingToProcess.add(booking);
+		processInvoices();
+	}
+
+	private void processInvoices() {
+		Set<Booking> failedToProcess = new HashSet<>();
+		for (Booking booking : this.bookingToProcess) {
+			if (!booking.isCancelled()) {
+				if (booking.getPaymentReference() == null) {
+					try {
+						booking.setPaymentReference(
+								BankInterface.processPayment(booking.getIban(), booking.getAmount()));
+					} catch (BankException | RemoteAccessException ex) {
+						failedToProcess.add(booking);
+						continue;
+					}
+				}
+			}
+		}
+
+		this.bookingToProcess.clear();
+		this.bookingToProcess.addAll(failedToProcess);
+
+	}
+
+	public void clean() {
+		this.bookingToProcess.clear();
+	}
+
+}
