11525680fc91e8ba8541e8c6884c3cdf573c7295
Andreia Valente <andreiaavalentee@gmail.com>
2018-04-21 22:17:40 +0100

    Found better way of writing the relation between the Invoice and Buyer and Seller. Also added the relation between the Invoice and ItemType. (references #112 )

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Invoice.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Invoice.java
@@ -8,8 +8,6 @@ import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
 
 public class Invoice extends Invoice_Base{
 	private static int counter = 0;
-	private final ItemType itemType;
-
 	private boolean cancelled = false;
 
 	Invoice(double value, LocalDate date, ItemType itemType, Seller seller, Buyer buyer) {
@@ -18,9 +16,9 @@ public class Invoice extends Invoice_Base{
 		super.setReference(Integer.toString(++Invoice.counter));
 		super.setValue(value);
 		super.setDate(date);
-		this.itemType = itemType;
-		super.setSeller(seller);
-		super.setBuyer(buyer);
+		super.setItemType(itemType);
+		super.addTaxPayer(seller);
+		super.addTaxPayer(buyer);
 		super.setIva(value * itemType.getTax() / 100);
 
 		seller.addInvoice(this);
@@ -49,11 +47,23 @@ public class Invoice extends Invoice_Base{
 		}
 	}
 
-	public ItemType getItemType() {
-		return this.itemType;
+	public TaxPayer getSeller() {
+		for (TaxPayer tp : getTaxPayerSet()) {
+			if (tp.getClass() == Seller.class) {
+				return tp;
+			}
+		}
+		return null;
 	}
 
-
+	public TaxPayer getBuyer() {
+		for (TaxPayer tp : getTaxPayerSet()) {
+			if (tp.getClass() == Buyer.class) {
+				return tp;
+			}
+		}
+		return null;
+	}
 
 	public void cancel() {
 		this.cancelled = true;
@@ -63,11 +73,7 @@ public class Invoice extends Invoice_Base{
 		return this.cancelled;
 	}
 
-
-
 	public void delete() {
-		setSeller(null);
-		setBuyer(null);
 		deleteDomainObject();
 	}
 }
